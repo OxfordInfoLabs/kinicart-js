c7f688631cbc79854718a1b2ffd3f2a5b44ec803
Mark Robertshaw <mark@oxil.co.uk>
2020-03-17 16:31:07 +0000

    New add to cart logic

--- ts/components/kc-packaged-product.ts
+++ ts/components/kc-packaged-product.ts
@@ -1,5 +1,6 @@
 import Api from '../framework/api';
 import Kiniauth from "../../../kiniauth-js/ts/index";
+import Configuration from "../../../kiniauth-js/ts/configuration";
 
 export default class KcPackagedProduct extends HTMLElement {
 
@@ -11,6 +12,7 @@ export default class KcPackagedProduct extends HTMLElement {
 
 
     private bind() {
+
         const productIdentifier = this.getAttribute('data-product-identifier');
         const view = Kiniauth.kinibind.bind(this, {
             plans: {},
@@ -21,6 +23,48 @@ export default class KcPackagedProduct extends HTMLElement {
         api.getPackageProductPlans(productIdentifier).then(res => {
             view.models.plans = res;
         });
+
+
+        this.querySelectorAll("[data-add-to-cart]").forEach((item) => {
+
+            item.addEventListener("click", event => {
+
+
+                let addToCart = <HTMLElement>event.target;
+
+                let cartItemDescriptor = {
+                    planIdentifier: addToCart.getAttribute("data-plan"),
+                    addOnDescriptors: []
+                };
+
+                this.doAddToCart(productIdentifier, cartItemDescriptor).then(() => {
+                    let destination = addToCart.getAttribute("data-destination");
+                    window.location.href = destination ? destination : "/cart";
+                });
+
+            });
+
+        });
+
+
+    }
+
+
+    // Do add to cart
+    private doAddToCart(productIdentifier: string, addDescriptor: any): Promise<any> {
+
+        let api = new Api();
+
+        const addURL = this.getAttribute('data-add-url');
+        if (!addURL) {
+            return api.addProductToCart(productIdentifier, addDescriptor, this.getAttribute("data-cart-item-index"));
+        } else {
+            return fetch(Configuration.endpoint + addURL, {
+                method: 'POST',
+                credentials: 'include',
+                body: JSON.stringify(addDescriptor)
+            });
+        }
 
 
     }
